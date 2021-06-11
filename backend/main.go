e77f2c7d42b2834e48cada2884ce0aaded6b0cad
rifqi23ahmad <rifqi23ahmad@gmail.com>
2021-06-11 17:00:48 +0700

    commit ke8

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"backend/handler"
+	"backend/user"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	DB             *gorm.DB = config.Connect()
+	userRepository          = user.NewRepository(DB)
+	userService             = user.NewService(userRepository)
+	userHandler             = handler.NewUserHandler(userService)
+)
 
 func main() {
 
 	r := gin.Default()
+	r.GET("/user", userHandler, ShowUser)
 
-	r.Run()
+	r.Run("8080")
 
 }
