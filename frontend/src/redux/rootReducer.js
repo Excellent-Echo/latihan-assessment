adc76145c8a21f3d3ed0ccc2b9ac1aba406ce17c
marwanjuna <mrwnolii@gmail.com>
2021-06-11 16:22:23 +0700

    form register on progress

---
+++ frontend/src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import userRegisterReducer from "./user/register/userRegisterReducer";
+
+const rootReducer = combineReducers({
+  userRegister: userRegisterReducer,
+});
+
+export default rootReducer;
