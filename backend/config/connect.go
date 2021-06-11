b99f0db033c949ad42eefba4d2c98293f395532e
rizkyran <rizkyran.rr@gmail.com>
2021-06-11 10:36:43 +0700

    second commit (release 2 proses)

---
+++ backend/config/connect.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Conn() *gorm.DB {
+	dsn := "root:@tcp(localhost)/book_assesment?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+}
--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+
+	"latihan-assessment/backend/config"
+)
+
+var DB *gorm.DB = config.Conn()
+
 func main() {
+	r := gin.Default()
+
+	r.GET("/users", HandleUsers)
+	r.POST("/users/register")
+	r.POST("/users/login")
+	r.GET("/users/:id")
+	r.PUT("/users/:id")
+	r.DELETE("/users/:id")
+
+	r.GET("/books", HandleBook)
+	r.POST("/books/")
+	r.GET("/books/:id")
+	r.PUT("/books/:id")
+	r.DELETE("/books/:id")
+
+	r.Run(":8080")
+}
+
+func HandleUsers(c *gin.Context) {
+	var users []Users
+
+	if err := DB.Find(&users).Error; err != nil {
+		log.Println(err.Error())
+	}
+
+	c.JSON(http.StatusOK, users)
+}
+
+func HandleBook(c *gin.Context) {
 
 }
