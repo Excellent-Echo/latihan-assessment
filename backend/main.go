645d80b91e41389b448887e7355e21c3a8682a09
helyus1412 <helyus1412@gmail.com>
2021-06-16 01:38:30 +0700

    update config swagger

--- backend/main.go
+++ backend/main.go
@@ -18,9 +18,9 @@ func main() {
 	docs.SwaggerInfo.Title = "book list API documentation"
 	docs.SwaggerInfo.Description = "book list API documentation"
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "https://book-list-helmi.herokuapp.com/"
+	docs.SwaggerInfo.Host = "book-list-helmi.herokuapp.com"
 	docs.SwaggerInfo.BasePath = ""
-	docs.SwaggerInfo.Schemes = []string{"http"}
+	docs.SwaggerInfo.Schemes = []string{"https"}
 
 	//routes API
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
